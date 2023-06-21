44b7b09452d38e40550d4e7ceed806a8b737d5c6
yosetc <ycozco@unsa.edu.pe>
2023-06-20 20:03:22 -0500

    cifrado cesar actualizado

--- lab02/cifradoCesar/main2.py
+++ lab02/cifradoCesar/main2.py
@@ -52,10 +52,14 @@ class CifradoCesar:
 
     @staticmethod
     def from_file(ruta_archivo, desplazamiento):
-        with open(ruta_archivo, 'r') as archivo:
+        # Abrimos el archivo en modo lectura, especificando la codificación
+        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+            # Leemos el contenido del archivo
             texto_plano = archivo.read()
+            #print texto_plano con mensaje texto plano es
+            print("Texto plano: ", texto_plano)
+        # Ciframos el contenido del archivo y lo devolvemos
         return CifradoCesar(desplazamiento).cifrar(texto_plano)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Cifrado Cesar')
@@ -63,14 +67,16 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--text', help='El texto plano a ser cifrado')
     parser.add_argument('-s', '--shift', type=int, help='El desplazamiento para el Cifrado Cesar')
     args = parser.parse_args()
-
+    # si es un archivo lo que se va a cifrar entonces se ejecuta el metodo from_file
     if args.file:
-        texto_cifrado = CifradoCesar.from_file(args.file, args.shift)
-        print("Texto cifrado: ", texto_cifrado)
-        print("Texto descifrado: ", CifradoCesar(args.shift).descifrar(texto_cifrado))
+        cifrador = CifradoCesar.from_file(args.file, args.shift)
+    # si es un texto lo que se va a cifrar de frente se ejecuta el metodo cifrar
     elif args.text:
-        texto_cifrado = CifradoCesar(args.shift).cifrar(args.text)
-        print("Texto cifrado: ", texto_cifrado)
-        print("Texto descifrado: ", CifradoCesar(args.shift).descifrar(texto_cifrado))
+        cifrador = CifradoCesar(args.shift).cifrar(args.text)
+    # si no se proporciona un archivo o un texto para ser cifrado, se muestra el mensaje de error
     else:
         print("Por favor, proporciona un archivo o un texto para ser cifrado.")
+        exit()
+    # se muestra el texto cifrado y el texto descifrado
+    print("Texto cifrado: ", cifrador)
+    print("Texto descifrado: ", CifradoCesar(args.shift).descifrar(cifrador))
